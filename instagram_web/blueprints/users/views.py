88fb30d25553f0d8bd3581500c4c0668f9b5fe32
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-05-08 18:05:44 +0800

    added profile page, username and profile image to profile page, added users images to profile page

--- instagram_web/blueprints/users/views.py
+++ instagram_web/blueprints/users/views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 from flask_login import current_user, login_required
 from models.user import User
+from models.image import Image
 from werkzeug.security import generate_password_hash, check_password_hash
 import re
 
--- models/image.py
+++ models/image.py
@@ -1,11 +1,17 @@
 from models.base_model import BaseModel
 from models.user import User
 from flask_login import LoginManager, UserMixin
+from playhouse.hybrid import hybrid_property
 import peewee as pw
 import re
+from config import AWS_LINK
 
 
 class Image(BaseModel):
 
     user = pw.ForeignKeyField(User, backref='images')
     image = pw.CharField(null=True, default=None)
+
+    @hybrid_property
+    def users_image_url(self):
+        return f"{AWS_LINK}/{self.image}"

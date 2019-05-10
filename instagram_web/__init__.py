0dba59213fe4fe61ab6b33114b58ad0dc0190060
Sandra Bayabos <sandra.bayabos@gmail.com>
2019-05-10 10:52:54 +0800

    added google_oauth.py

--- instagram_web/__init__.py
+++ instagram_web/__init__.py
@@ -8,6 +8,7 @@ from instagram_web.blueprints.images.views import images_blueprint
 from instagram_web.blueprints.donations.views import donations_blueprint
 from flask_assets import Environment, Bundle
 from .util.assets import bundles
+from instagram.helpers.google_oauth import oauth
 
 assets = Environment(app)
 assets.register(bundles)
@@ -20,6 +21,9 @@ app.register_blueprint(donations_blueprint, url_prefix="/donations")
 #FLASK LOGIN FUNCTION#
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+# GOOGLE OAUTH
+oauth.init_app(app)
 
 #user_loader used to reload the user object from hte user ID stored in the session#
 
